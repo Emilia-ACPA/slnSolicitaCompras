dec6396dba3278201ae87c1c8f3288823183234d
dev <desenvolvimento@acpa.net.br>
2025-03-27 07:58:18 -0300

    Adicionar arquivos de projeto.

---
+++ prjSolicitaCompras/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace prjSolicitaCompras
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
