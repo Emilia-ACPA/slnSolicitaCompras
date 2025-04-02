6860037e50bf531ddf43f707d438f401282db716
dev <desenvolvimento@acpa.net.br>
2025-04-02 16:54:05 -0300

    Customização da tela NovaSolicitacao e tratamento de entrada de dados

---
+++ prjSolicitaCompras/DateMaskBehavior.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjSolicitaCompras
+{
+    class DateMaskBehavior : Behavior<Entry>
+    {
+        private const string DateFormat = "dd/MM/yyyy";
+        private const string Mask = "__/__/____";
+
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            bindable.TextChanged += OnTextChanged;
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            bindable.TextChanged -= OnTextChanged;
+            base.OnDetachingFrom(bindable);
+        }
+
+        private void OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var entry = sender as Entry;
+            if (entry == null)
+                return;
+
+            string text = entry.Text;
+
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                entry.Text = Mask;
+                return;
+            }
+
+            if (text.Length > Mask.Length)
+            {
+                entry.Text = text.Substring(0, Mask.Length);
+                return;
+            }
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (i < Mask.Length && Mask[i] != '_' && text[i] != Mask[i])
+                {
+                    text = text.Insert(i, Mask[i].ToString());
+                }
+            }
+
+            entry.Text = text;
+
+            if (DateTime.TryParseExact(text, DateFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime parsedDate))
+            {
+                entry.BackgroundColor = Colors.Transparent;
+            }
+            else
+            {
+                entry.BackgroundColor = Colors.Red;
+            }
+        }
+    }
+}
--- prjSolicitaCompras/NovaSolicitacao.xaml.cs
+++ prjSolicitaCompras/NovaSolicitacao.xaml.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 namespace prjSolicitaCompras;
 
 public partial class NovaSolicitacao : ContentPage
@@ -6,4 +8,31 @@ public partial class NovaSolicitacao : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void EdUsuario_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        if (string.IsNullOrWhiteSpace(EdUsuario.Text))
+        {
+            EdUsuario.BackgroundColor = Colors.Red;
+        }
+        else
+        {
+            EdUsuario.BackgroundColor = Colors.Transparent;
+        }
+    }
+
+    private void DtSolicitacao_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        string dateFormat = "dd/MM/yyyy";
+        if (DateTime.TryParseExact(DtSolicitacao.Text, dateFormat, CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out DateTime parsedDate))
+        {
+            DtSolicitacao.BackgroundColor = Colors.Transparent;
+            DtSolicitacao.Text = parsedDate.ToString(dateFormat);
+        }
+        else
+        {
+            DtSolicitacao.BackgroundColor = Colors.Red;
+        }
+    }
+
 }
